0a605f430c4b786c79012aa564bff1dbed2b321d
Sergey.Kolotevsergey.kolotev@glowbyteconsulting.com <you@example.com>
2026-09-06 19:19:15 +0300

    fix: microservice tests

--- accounts/src/test/java/ru/yandex/practicum/accounts/AccountApplicationTests.java
+++ accounts/src/test/java/ru/yandex/practicum/accounts/AccountApplicationTests.java
@@ -2,8 +2,10 @@ package ru.yandex.practicum.accounts;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({"test","contract-test" })
 class AccountApplicationTests {
 
     @Test
