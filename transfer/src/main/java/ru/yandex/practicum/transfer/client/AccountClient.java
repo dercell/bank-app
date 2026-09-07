74845c98fbec5819737ba4162c08e02467c8ef1d
Sergey.Kolotevsergey.kolotev@glowbyteconsulting.com <you@example.com>
2026-09-07 13:13:55 +0300

    fix: second major review comment

--- transfer/src/main/java/ru/yandex/practicum/transfer/client/AccountClient.java
+++ transfer/src/main/java/ru/yandex/practicum/transfer/client/AccountClient.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.transfer.client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
 import ru.yandex.practicum.transfer.dto.ServiceResultDto;
 
@@ -17,18 +18,26 @@ public class AccountClient {
     }
 
     public ServiceResultDto transfer(String from, String to, int sum) {
-        return webClient
-                .put().uri(uriBuilder -> uriBuilder
-                        .path("/accounts/transfer")
-                        .queryParam("from", from)
-                        .queryParam("to", to)
-                        .queryParam("sum", sum)
-                        .build())
-                .header("Content-Type", "application/json")
-                .retrieve()
-                .bodyToMono(ServiceResultDto.class)
-                .onErrorResume(throwable -> Mono.just(new ServiceResultDto("Ошибка при обращении к account-service: " + throwable.getMessage())))
-                .block();
+        try {
+            return webClient
+                    .put().uri(uriBuilder -> uriBuilder
+                            .path("/accounts/transfer")
+                            .queryParam("from", from)
+                            .queryParam("to", to)
+                            .queryParam("sum", sum)
+                            .build())
+                    .header("Content-Type", "application/json")
+                    .retrieve()
+                    .bodyToMono(ServiceResultDto.class)
+                    .block();
+        } catch (WebClientResponseException e) {
+            log.error("WebClientResponseException in AccountClient transfer: {}", e.getMessage(), e);
+            throw e;
+        } catch (Exception error) {
+            log.error("Error while transfer: {}", error.getMessage(), error);
+            throw error;
+        }
+
     }
 
 }
